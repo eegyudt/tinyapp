5488ccdccb0e0cfe5fd0e329816fce153ce3d472
Eszter Egyud <eszter.egyud@telus.com>
2022-11-28 20:13:53 -0700

    Added random code function

--- express_server.js
+++ express_server.js
@@ -9,6 +9,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true })); //added for POST requests
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+// generate a random 6 character alphanumeric string
+let randomString = function() {
+  const inputArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let randomStr = '';
+  for (let i = 0; i <= 6; i++) {
+    let randomNum = Math.floor(Math.random() * inputArr.length);
+    randomStr = randomStr + inputArr[randomNum];
+  }
+  return randomStr;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
