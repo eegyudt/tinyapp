f0ef3bd1e6407eb3f9406f0d5995f9d8b1bfcece
Eszter Egyud <eszter.egyud@telus.com>
2022-12-01 01:28:53 -0700

    partially implemented listing user's own urls

--- express_server.js
+++ express_server.js
@@ -7,10 +7,25 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true })); //added for POST requests
 
 
-//database for random code - url key-value pairs
+// //database for random code - url key-value pairs
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  ixVoGr: {
+    longURL: "https://www.apple.ca",
+    userID: "aJ50lW",
+  },
 };
 
 //database for users
@@ -46,6 +61,21 @@ const getUserByEmail = function(userEmail) {
   return foundUser;
 };
 
+//
+const urlsForUser = function(id) {
+  const urlObject = {};
+  for (let key in urlDatabase) {
+    if (key.userID === id) {
+      const valueObj = {
+        longURL: urlDatabase[key].longURL,
+        userID: urlDatabase[key].userID
+      };
+      urlObject[key] = valueObj;
+    }
+  }
+  return urlObject;
+};
+
 // Rendering home page
 app.get("/", (req, res) => {
   res.redirect('/urls');
@@ -55,13 +85,14 @@ app.get("/", (req, res) => {
 // API/data handler routes
 // Create - POST
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
   const user_id = req.cookies['user_id'];
+
   if (!user_id) {
     return res.status(400).send("You must be logged in to use TinyApp!");
   }
   const urlShortCode = randomString();
-  urlDatabase[urlShortCode] = req.body.longURL;
+  urlDatabase[urlShortCode].longURL = req.body.longURL;
+  urlDatabase[urlShortCode].userID = user_id;
 
   res.redirect(`/urls/${urlShortCode}`); //redirecting from shortURL to longURL
 });
@@ -69,19 +100,20 @@ app.post("/urls", (req, res) => {
 // Read a short URL - GET
 app.get("/u/:id", (req, res) => {
   if (urlDatabase[req.params.id]) {
-    const longURL = urlDatabase[req.params.id];
+    const longURL = urlDatabase[req.params.id].longURL;
     if (!longURL) {
       res.status(400).send("This short url does not exist!");
     } else {
       res.redirect(longURL);
     }
- }});
+  }
+});
 
 //Update - POST
 app.post('/urls/:id', (req, res) => {
   const urlId = req.params.id;
   const longURL = req.body.longURL;
-  urlDatabase[urlId] = longURL;
+  urlDatabase[urlId].longURL = longURL;
   res.redirect('/urls');
 });
 
@@ -113,7 +145,7 @@ app.get("/urls/:id", (req, res) => {
 
   const templateVars = {
     id: req.params.id,
-    longURL: urlDatabase[req.params.id],
+    longURL: urlDatabase[req.params.id].longURL,
     user
   };
   res.render("urls_show", templateVars);
@@ -145,8 +177,14 @@ app.get("/login", (req, res) => {
 //All - GET
 app.get("/urls", (req, res) => {
   const user_id = req.cookies['user_id'];
+  if (!user_id) {
+    return res.status(400).send("You must login first!");
+  }
+
+  const urlObj = urlsForUser(user_id);
+
   const user = users[user_id];
-  const templateVars = { user, urls: urlDatabase };
+  const templateVars = { user, urls: urlObj};
   res.render("urls_index", templateVars);
 });
 
@@ -183,7 +221,6 @@ app.post('/register', (req, res) => {
 
 //login route - POST
 app.post('/login', (req, res) => {
-  console.log(users);
   const email = req.body.email;
   const password = req.body.password;
   if (!email || !password) {
